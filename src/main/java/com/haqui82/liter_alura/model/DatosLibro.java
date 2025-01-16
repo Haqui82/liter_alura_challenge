790d0b2cb22dd647124e3ddc37c3d10819e3f453
Haqui82 <mr.haqui82@gmail.com>
2025-01-16 06:19:20 -0500

    mapping all DatosLibro atributes

--- src/main/java/com/haqui82/liter_alura/model/DatosLibro.java
+++ src/main/java/com/haqui82/liter_alura/model/DatosLibro.java
@@ -3,43 +3,82 @@ package com.haqui82.liter_alura.model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record DatosLibro(
-        @JsonAlias("title") String titulo,
-        @JsonAlias("name")String autor,
-        @JsonAlias("birth_year")Integer nacimientoAutor,
-        @JsonAlias("death_year")Integer fallecimientoAutor) {
+import java.util.List;
 
-    @Override
-    public String titulo() {
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class DatosLibro {
+
+    @JsonAlias("title")
+    private String titulo;
+
+    @JsonAlias("authors")
+    private List<Autor> autores;
+
+    // Constructor, getters y setters
+
+    public String getTitulo() {
         return titulo;
     }
 
-    @Override
-    public String autor() {
-        return autor;
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
     }
 
-    @Override
-    public Integer nacimientoAutor() {
-        return nacimientoAutor;
+    public List<Autor> getAutores() {
+        return autores;
     }
 
-    @Override
-    public Integer fallecimientoAutor() {
-        return fallecimientoAutor;
-    }
-
-    public DatosLibro {
+    public void setAutores(List<Autor> autores) {
+        this.autores = autores;
     }
 
     @Override
     public String toString() {
+        Autor autor = autores.get(0); // Asumiendo que siempre hay al menos un autor
         return "DatosLibro{" +
                 "titulo='" + titulo + '\'' +
-                ", autor='" + autor + '\'' +
-                ", nacimientoAutor=" + nacimientoAutor +
-                ", fallecimientoAutor=" + fallecimientoAutor +
+                ", autor='" + autor.getNombre() + '\'' +
+                ", nacimientoAutor=" + autor.getNacimientoAutor() +
+                ", fallecimientoAutor=" + autor.getFallecimientoAutor() +
                 '}';
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Autor {
+
+        @JsonAlias("name")
+        private String nombre;
+
+        @JsonAlias("birth_year")
+        private Integer nacimientoAutor;
+
+        @JsonAlias("death_year")
+        private Integer fallecimientoAutor;
+
+        // Constructor, getters y setters
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public Integer getNacimientoAutor() {
+            return nacimientoAutor;
+        }
+
+        public void setNacimientoAutor(Integer nacimientoAutor) {
+            this.nacimientoAutor = nacimientoAutor;
+        }
+
+        public Integer getFallecimientoAutor() {
+            return fallecimientoAutor;
+        }
+
+        public void setFallecimientoAutor(Integer fallecimientoAutor) {
+            this.fallecimientoAutor = fallecimientoAutor;
+        }
     }
 }
